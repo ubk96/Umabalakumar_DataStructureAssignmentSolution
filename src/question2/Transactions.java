f92f99ec88e1bc0a2dec48364973cfc2942387b9
UMABALAKUMAR <34569038+ubk96@users.noreply.github.com>
2022-07-29 21:38:32 +0530

    Update Transactions.java

--- src/question2/Transactions.java
+++ src/question2/Transactions.java
@@ -1,10 +1,92 @@
 package question2;
 
+//import java.io.*;
+
+class Node
+{
+    int val;
+    Node left, right;
+    
+    Node(int item)
+    {
+        val = item;
+        left = right = null;
+    }
+}
+
 public class Transactions {
+	 	
+	public static Node node;
+    static Node prevNode = null;
+    static Node headNode = null;
+   
+    static void flattenBTToSkewed(Node root,
+                                  int order)
+    {
+       
+        if(root == null)
+        {
+            return;
+        }
+       
+        if(order > 0)
+        {
+            flattenBTToSkewed(root.right, order);
+        }
+        else
+        {
+            flattenBTToSkewed(root.left, order);
+        }
+        Node rightNode = root.right;
+        Node leftNode = root.left;
+       
+        if(headNode == null)
+        {
+            headNode = root;
+            root.left = null;
+            prevNode = root;
+        }
+        else
+        {
+            prevNode.right = root;
+            root.left = null;
+            prevNode = root;
+        }
+       
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        if (order > 0)
+        {
+            flattenBTToSkewed(leftNode, order);
+        }
+        else
+        {
+            flattenBTToSkewed(rightNode, order);
+        }
+    }
+   
 
-	}
+    static void traverseRightSkewed(Node root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        System.out.print(root.val + " ");
+        traverseRightSkewed(root.right);       
+    }
+   
+    // Driver Code
+    public static void main (String[] args)
+    {
 
+    	Transactions tree = new Transactions();
+        tree.node = new Node(5);
+        tree.node.left = new Node(3);
+        tree.node.right = new Node(6);
+       
+
+        int order = 0;
+        flattenBTToSkewed(node, order);
+        traverseRightSkewed(headNode);
+    }
 }
