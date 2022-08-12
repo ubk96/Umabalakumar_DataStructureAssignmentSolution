2c5f8039eab93e12715bb74949f53c870d527d8e
UMABALAKUMAR <34569038+ubk96@users.noreply.github.com>
2022-08-12 19:48:14 +0530

    Umabalakumar_DataStructureAssignmentSolution

--- src/question1/Skyscraper.java
+++ src/question1/Skyscraper.java
@@ -2,49 +2,37 @@ package question1;
 
 import java.util.Scanner;
 
-import question1.Linkedlist.Node;
-
 public class Skyscraper {
 
 	    public static void main(String[] args)
 	    {
-	    	Linkedlist list = new Linkedlist();
-	    	
-			try(Scanner sc = new Scanner(System.in)){
-		        
-				System.out.println("enter the total no of floors in the building");
-		        
-				int no_of_floors = sc.nextInt();
-		        int Bottom_floor = no_of_floors, j=1;
-		        boolean flag;
-		        
-		        for(int i=0; i<no_of_floors; i++) {
-		        	System.out.println("enter the floor size given on day :" + (i+1));
-		        	list = list.insert(list, sc.nextInt());
-		        }
-		        
-		        System.out.println("The order of construction is as follows ");
-		        
-//		        list.head = list.reverse(list.head);
-		        Node current = list.head;
-		        
-		        for(int i=no_of_floors;i>=1;i--){
-		            flag=false;
-		            System.out.println("Day "+j+" :");
-		            j++;
-		            while(Bottom_floor>=1 && current.data>=i){
-		                flag=true;
-		                System.out.print(Bottom_floor +" ");
-		                Bottom_floor--;
-		                current = current.next;
-		            }
-		            if(flag==true){
-		                System.out.println();
-//		                if((current.next) != null) {
-//		                current = current.next;
-//		                }
-		            }
-		        }
-			}
-	    }
+				try(Scanner sc = new Scanner(System.in)){
+		        	System.out.println("Enter the total number floors in the building : ");
+		        	int n = sc.nextInt();
+			        int x[] = new int[n+1];
+			        
+			        for(int i=1;i<n+1;i++){
+			            System.out.println("Enter the floor size given on day : "+i);
+			            int m=sc.nextInt();
+			            x[m]=i;
+			        }
+			        int j=n;
+			        boolean flag;
+			        
+			        
+			        System.out.println("The order of construction is as follows ");
+			        for(int i=1;i<=n;i++){
+			            flag=false;
+			            System.out.println("Day "+i+" :");
+			            while(j>=1 && x[j]<=i){
+			                flag=true;
+			                System.out.print(j +" ");
+			                j--;
+			            }
+			            if(flag==true){
+			                System.out.println();
+			         }
+			     }
+		    }
+	  }
 }
